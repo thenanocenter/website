7a11b127eb925fc4badd3485f9aa2b6bba7bc6f5
Jeff Kilroy <jeffkilroy@gmail.com>
2018-06-25 11:55:26 -0400

    footer styles

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="container-inner">
             <div class="footer-inner d-flex justify-content-between align-content-center">
                 <div class="align-self-center"><span class="logo"><?php echo file_get_contents(public_path('/img/logo-blue.svg')); ?></span></div>
-                <div class="align-self-center"><a href="#">Start using Nano <span class="icon-grey"><?php echo file_get_contents(public_path('/img/arrow-right-white.svg')); ?></span></a></div>
+                <div class="align-self-center"><a href="#" class="text-grey">Start using Nano <span class="icon-grey"><?php echo file_get_contents(public_path('/img/arrow-right-white.svg')); ?></span></a></div>
             </div>
         </div>
     </div>
