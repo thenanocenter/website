de8b36b1d495dfd7e35659da484237762646d366
Jeff Kilroy <jeffkilroy@gmail.com>
2018-06-25 07:01:12 -0400

    header+mobile nav styles

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -1,17 +1,18 @@
-
-    <nav class="navbar navbar-expand-md navbar-dark">
-        <div class="container">
-            <div class="container-inner d-flex justify-content-between w-100">
+<nav class="navbar navbar-expand-md navbar-dark">
+    <div class="container">
+        <div class="container-inner">
+            <div class="branding-bar">
                 <a class="navbar-brand align-self-center" href="{{ url('/') }}">
                     <?php echo file_get_contents(public_path('/img/icon-white.svg')); ?>
                 </a>
-
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMenu"
+                        aria-controls="navbarMenu" aria-expanded="false"
+                        aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
-                <div class="collapse navbar-collapse d-flex justify-content-center flex-grow-1" id="navbarSupportedContent">
-
+            </div>
+            <div class="collapse navbar-collapse" id="navbarMenu">
+                <div class="collapse-inner">
                     <ul class="navbar-nav flex-grow-1 justify-content-center">
                         @include('layouts.partials.nav-items-center')
                     </ul>
@@ -21,13 +22,13 @@
                     </ul>
 
                     @auth
-                    <div class="navbar-nav navbar-admin">
-                        @include('layouts.partials.nav-items-admin')
-                    </div>
+                        <div class="navbar-nav navbar-admin">
+                            @include('layouts.partials.nav-items-admin')
+                        </div>
                     @endauth
-
                 </div>
             </div>
         </div>
-    </nav>
+    </div>
+</nav>
 
