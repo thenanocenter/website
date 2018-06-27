0021205842814e54e0a072b7edde94b53d929fde
Jeff Kilroy <jeffkilroy@gmail.com>
2018-06-27 10:12:30 -0400

    Link start to faucet

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="container-inner">
             <div class="footer-inner d-flex justify-content-between align-content-center">
                 <div class="align-self-center"><span class="logo"><?php echo file_get_contents(public_path('/img/logo-blue.svg')); ?></span></div>
-                <div class="align-self-center"><a href="{{ url('/start') }}" class="text-grey">Start using Nano <span class="icon-grey"><?php echo file_get_contents(public_path('/img/arrow-right-white.svg')); ?></span></a></div>
+                <div class="align-self-center"><a href="https://www.nanofaucet.org" target="_blank" class="text-grey">Start using Nano <span class="icon-grey"><?php echo file_get_contents(public_path('/img/arrow-right-white.svg')); ?></span></a></div>
             </div>
         </div>
     </div>
