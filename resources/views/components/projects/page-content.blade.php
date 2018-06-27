939f0c661166382f446a8cf2fa9f8ba15775a901
Jeff Kilroy <jeffkilroy@gmail.com>
2018-06-27 16:53:46 -0400

    bigger goal note

--- resources/views/components/projects/page-content.blade.php
+++ resources/views/components/projects/page-content.blade.php
@@ -1,11 +1,11 @@
 <div class="project-page-content">
     @if(!empty($project->goal))
-        <div class="mb-1 text-muted text-right">
+        <h5 class="mb-1 text-muted text-right">
             <strong>Goal:</strong> {{ $project->goal }}
             @if(!empty($project->progress_percentage))
                 <em>({{ $project->progress_percentage }}% Funded)</em>
             @endif
-        </div>
+        </h5>
     @endif
     <div class="project-page-description">
         {!! $project->description_html !!}
