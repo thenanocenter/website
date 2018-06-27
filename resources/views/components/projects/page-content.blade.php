24906818ba4ceac83f0d5f3beb3fd8e9ca231771
Jeff Kilroy <jeffkilroy@gmail.com>
2018-06-27 14:36:11 -0400

    Project page cleanup

--- resources/views/components/projects/page-content.blade.php
+++ resources/views/components/projects/page-content.blade.php
@@ -1,13 +1,13 @@
-@if(!empty($project->goal))
-    <div class="mb-1 text-muted text-right"><strong>Goal:</strong> {{ $project->goal }} @if(!empty($project->progress_percentage))<em>({{ $project->progress_percentage }}% Funded)</em>@endif</div>
-@endif
-<div class="row">
-    <div class="col-sm-4">
-        <img class="card-img-left flex-auto d-none d-lg-block" style="max-width: 250px;" alt="{{ $project->name }}" src="{{ asset('storage/'. $project->image_path ) }}">
-    </div>
-    <div class="col-sm-8">
-        <div>
-            {!! $project->description_html !!}
+<div class="project-page-content">
+    @if(!empty($project->goal))
+        <div class="mb-1 text-muted text-right">
+            <strong>Goal:</strong> {{ $project->goal }}
+            @if(!empty($project->progress_percentage))
+                <em>({{ $project->progress_percentage }}% Funded)</em>
+            @endif
         </div>
+    @endif
+    <div class="project-page-description">
+        {!! $project->description_html !!}
     </div>
 </div>
