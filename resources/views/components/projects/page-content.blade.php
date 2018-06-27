9be63198f20398bf3c46a73c030bfa54d3b445c2
Jeff Kilroy <jeffkilroy@gmail.com>
2018-06-27 09:33:36 -0400

    move goal above content block

--- resources/views/components/projects/page-content.blade.php
+++ resources/views/components/projects/page-content.blade.php
@@ -1,11 +1,11 @@
+@if(!empty($project->goal))
+    <div class="mb-1 text-muted text-right"><strong>Goal:</strong> {{ $project->goal }} @if(!empty($project->progress_percentage))<em>({{ $project->progress_percentage }}% Funded)</em>@endif</div>
+@endif
 <div class="row">
     <div class="col-sm-4">
         <img class="card-img-left flex-auto d-none d-lg-block" style="max-width: 250px;" alt="{{ $project->name }}" src="{{ asset('storage/'. $project->image_path ) }}">
     </div>
     <div class="col-sm-8">
-        @if(!empty($project->goal))
-        <div class="mb-1 text-muted text-right"><strong>Goal:</strong> {{ $project->goal }} @if(!empty($project->progress_percentage))<em>({{ $project->progress_percentage }}% Funded)</em>@endif</div>
-        @endif
         <div>
             {!! $project->description !!}
         </div>
