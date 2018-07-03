0327a8a203ae9f2a093cfdf8312b8bddba410ba4
Jeff Kilroy <jeffkilroy@gmail.com>
2018-07-03 06:43:13 -0400

    footer columns

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="footer bg-white">
     <div class="container">
         <div class="container-inner">
-            <div class="footer-inner d-flex justify-content-between align-content-center">
+            <div class="footer-inner d-flex flex-column flex-md-row justify-content-between align-content-center">
                 <div class="align-self-center"><span class="logo"><?php echo file_get_contents(public_path('/img/logo-blue.svg')); ?></span></div>
                 <div class="align-self-center"><a href="https://www.nanofaucet.org" target="_blank" class="text-grey">Start using Nano <span class="icon-grey"><?php echo file_get_contents(public_path('/img/arrow-right-white.svg')); ?></span></a></div>
             </div>
