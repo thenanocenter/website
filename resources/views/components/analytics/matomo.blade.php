5282343fe5eddcd84b72e2e9742fcb92db9ff1b5
Jeff Kilroy <jeffkilroy@gmail.com>
2018-07-03 10:50:13 -0400

    add matomo

---
+++ resources/views/components/analytics/matomo.blade.php
@@ -0,0 +1,19 @@
+<!-- Matomo -->
+<script type="text/javascript">
+    var _paq = _paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+    _paq.push(["setCookieDomain", "*.nanocenter.org"]);
+    _paq.push(["setDoNotTrack", true]);
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+        var u="https://skubuild.com/matomo/";
+        _paq.push(['setTrackerUrl', u+'piwik.php']);
+        _paq.push(['setSiteId', '4']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    })();
+</script>
+<noscript><p><img src="https://skubuild.com/matomo/piwik.php?idsite=4&amp;rec=1" style="border:0;" alt="" /></p></noscript>
+<!-- End Matomo Code -->
--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -4,5 +4,6 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <title>{{ config('app.name') }}</title>
 @include('components.analytics.gtag')
+@include('components.analytics.matomo')
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 <link rel="stylesheet" href="https://use.typekit.net/mac2ouo.css">
