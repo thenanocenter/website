fbf7e18ee8a474802388a9fcf882251a846d7d09
Jeff Kilroy <jeffkilroy@gmail.com>
2018-07-09 08:48:10 -0400

    link to sample proposal

--- resources/views/projects/proposal/index.blade.php
+++ resources/views/projects/proposal/index.blade.php
@@ -18,7 +18,7 @@
     <div class="col-sm-8 offset-sm-2">
         {!! Former::textarea('description','')->placeholder('Project Description*')->rows(10) !!}
         {!! Former::text('goal','')->placeholder('Estimated Project Goal*')->required()->help('The estimated amount required for this project (Be sure to specify currency - Nano, USD, Euro...)') !!}
-        {!! Former::text('written_proposal_url','')->placeholder('Written Proposal URL*')->required()->help('A link to a well documented proposal. This is required for acceptance. This may be a link to an external PDF or Google Docs file. <a href="#" target="_blank">For an example, click here</a>') !!}
+        {!! Former::text('written_proposal_url','')->placeholder('Written Proposal URL*')->required()->help('A link to a well documented proposal. This is required for acceptance. This may be a link to an external PDF or Google Docs file. <a href="https://docs.google.com/document/d/1qfywO6s5wFLOuUDRIgmtsWbuoAnCkgmXcGWTyH8UAww" target="_blank">For an example, click here</a>') !!}
         {!! Former::textarea('links','')->placeholder('Any additional links to project media, presentations, or guidelines')->help('A link to a well documented proposal. This is required for acceptance. This may be a link to an external PDF or Google Docs file. <a href="#" target="_blank">For an example, click here</a>') !!}
         @include('components.recaptcha.widget')
     </div>
