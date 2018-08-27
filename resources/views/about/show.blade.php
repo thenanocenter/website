f3523a12361064577bcd669378b81e642c78a88f
Jason Wen <jason@leadsocial.com>
2018-08-27 15:43:56 +0200

    Improved spacing and breakpoints

--- resources/views/about/show.blade.php
+++ resources/views/about/show.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
     <div class="row">
-        <div class="col-sm-8">
+        <div class="col-sm-8 col-lg-6">
             <h1>About NanoCenter</h1>
-            <h5>The Nano Center is a community born collective with the express aim of promoting Nano and its virtues to as wider audience as possible.</h5>
-            <p>Through carefully selected and community funded projects, we intend to foster adoption and integration of this revolutionary technology into the modern cryptocurrency space and beyond. We are very passionate about the qualities of this pure cryptocurrency as it can create a greener economy, bring stability to areas of economic volatility and provide people without access to conventional banking a way of controlling their money. Nano will empower the people of the world to take back control of their money. </p>
+            <h5 class="mt-5">The Nano Center is a community born collective with the express aim of promoting Nano and its virtues to as wider audience as possible.</h5>
+            <p class="mt-3">Through carefully selected and community funded projects, we intend to foster adoption and integration of this revolutionary technology into the modern cryptocurrency space and beyond. We are very passionate about the qualities of this pure cryptocurrency as it can create a greener economy, bring stability to areas of economic volatility and provide people without access to conventional banking a way of controlling their money. Nano will empower the people of the world to take back control of their money. </p>
             <p>The Nano Center was created by the community, for the community and we invite all that share our ambitions to join us and help in the effort. Everybody can contribute in their unique way to this mission, whether it be through a donation of time, funds, skills or ideas. We feel passionately that everyone who shares our vision had something valuable to bring to the cause.</p>
         </div>
     </div>
